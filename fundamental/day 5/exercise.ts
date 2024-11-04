7180a3c5b62ca86e93bd006969c2faab812668aa
randamopobela <randamopobela@gmail.com>
2024-11-04 22:41:39 +0900

    feat:soal 2

--- fundamental/day 5/exercise.ts
+++ fundamental/day 5/exercise.ts
@@ -2,9 +2,9 @@
 // a. Example : arr = [12, 5, 23, 18, 4, 45, 32] → {lowest : 4, highest: 45, average: 19.8xxx}
 
 let arr: number[] = [12, 5, 23, 18, 4, 45, 32];
-let lowest = arr[0];
-let highest = arr[0];
-let average = arr[0];
+let lowest: number = arr[0];
+let highest: number = arr[0];
+let average: number = arr[0];
 
 function getNum(arr: number[]) {
   let sum: number = 0;
@@ -25,4 +25,17 @@ function getNum(arr: number[]) {
 
 console.log(getNum(arr));
 
-//
+// // Write a function that takes an array of words and returns a string by concatenating the words in the array,
+// // separated by commas and - the last word - by an 'and'.
+// // a. Example : arr = ["apple", "banana", "cherry", "date"] → “apple,banana,cherry, and date”
+
+let arrString: string[] = ["apple", "banana", "cherry", "date", "watermelon"];
+
+function joinWords(arrString: string[]): string {
+  const word = arrString.slice(0, -1).join(",");
+  const lastWord = arrString[arrString.length - 1];
+
+  return `${word}, and ${lastWord}`;
+}
+
+console.log(joinWords(arrString));
