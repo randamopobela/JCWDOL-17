18d74b7dfd684c65e28b394993152348c85e07ca
randamopobela <randamopobela@gmail.com>
2024-11-05 12:46:06 +0900

    feat:exercise 1 day 4

--- fundamental/day 4/exercise4.ts
+++ fundamental/day 4/exercise4.ts
@@ -26,3 +26,32 @@ function triangle(height: number) {
 }
 
 triangle(4);
+
+// Create a function that can loop the number of times according to the input we provide, and will
+// replace multiples of 3 with "Fizz"
+// , multiples of 5 with "Buzz", multiples of 3 and 5 with
+// "FizzBuzz"
+// .
+// ● Parameters : n → total looping
+// ○ Example: n = 6 →1, 2, Fizz, 4, Buzz, Fizz
+// ○ Example: n = 15 → 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 12, 13, 14, FizzBuzz
+
+function fizzBuzz(numbers: number) {
+    let hasil = "";
+
+    for (let index = 0; index <= numbers; index++) {
+        if (index % 3 == 0) {
+            hasil += `Fizz`;
+        } else if (index % 5 == 00) {
+            hasil += `Buzz`;
+        } else if ((index % 3 == (0 % index) % 5) == 0) {
+            hasil += `FizzBuzz`;
+        } else {
+            hasil += `${index} `;
+        }
+
+        console.log(hasil);
+    }
+}
+
+fizzBuzz(15);
