b624346e2d8f22892fbbf8fedab42397ca4f3f10
randamopobela <randamopobela@gmail.com>
2024-12-11 23:33:44 +0900

    feat: update set koneksi contentful

--- Module 02 - Frontend/day 10/cms/src/app/page.tsx
+++ Module 02 - Frontend/day 10/cms/src/app/page.tsx
@@ -4,7 +4,20 @@ import {
     IContentfulAsset,
 } from "@/contentful/types/blogPost.types";
 
-export default function Home() {
+const getBlogPostsContenful = async () => {
+    try {
+        const data = await contentfulClient.getEntries<TypeBlogPostSkeleton>(); // Method getEntries akan mengambil semua konten kita di contenful.
+        console.log(data);
+
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export default async function Home() {
+    const posts = await getBlogPostsContenful();
+
     return (
         <div>
             <div>HOME</div>
